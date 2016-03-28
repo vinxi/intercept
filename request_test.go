43521f2b84aa2da61a6d2dc732986613b525520f
Yohan Robert <yohan.robert@runtastic.com>
2016-03-28 13:34:58 +0200

    feat(test): Add test for ReadString

--- request_test.go
+++ request_test.go
@@ -1,10 +1,22 @@
 package intercept
 
 import (
+	"errors"
 	"github.com/nbio/st"
+	"io/ioutil"
 	"net/http"
+	"strings"
 	"testing"
 )
+
+var errRead = errors.New("read error")
+
+type errorReader struct {
+}
+
+func (r *errorReader) Read(p []byte) (int, error) {
+	return 0, errRead
+}
 
 func TestNewRequestModifier(t *testing.T) {
 	h := http.Header{}
@@ -13,4 +25,22 @@ func TestNewRequestModifier(t *testing.T) {
 	modifier := NewRequestModifier(req)
 	st.Expect(t, modifier.Request, req)
 	st.Expect(t, modifier.Header, h)
+}
+
+func TestReadString(t *testing.T) {
+	bodyStr := `{"hello":"bonjour"}`
+	strReader := strings.NewReader(bodyStr)
+	body := ioutil.NopCloser(strReader)
+	req := &http.Request{Header: http.Header{}, Body: body}
+	modifier := NewRequestModifier(req)
+	str, err := modifier.ReadString()
+	st.Expect(t, err, nil)
+	st.Expect(t, str, bodyStr)
+
+	body = ioutil.NopCloser(&errorReader{})
+	req = &http.Request{Header: http.Header{}, Body: body}
+	modifier = NewRequestModifier(req)
+	str, err = modifier.ReadString()
+	st.Expect(t, err, errRead)
+	st.Expect(t, str, "")
 }
