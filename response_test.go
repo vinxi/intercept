8781ab90efa33e08b722a06bbe54ac5f85b43c1b
Yohan Robert <yohan.robert@outlook.fr>
2016-04-06 23:33:05 +0200

    Fix ResponseModifier.Reader
    * test(response): add test for JSON method with struct params
    
    * test(response): add test for JSON method with string params
    
    * test(response): add test for JSON method with byte array params
    
    * test(response): add tests for XML methods
    
    * fix(response): fix Reader method modiying request instead of response

--- response_test.go
+++ response_test.go
@@ -1,9 +1,11 @@
 package intercept
 
 import (
+	"bytes"
 	"encoding/json"
 	"encoding/xml"
 	"github.com/nbio/st"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -197,4 +199,121 @@ func TestResponseModifierByte(t *testing.T) {
 	body, err := ioutil.ReadAll(resp.Body)
 	st.Expect(t, err, nil)
 	st.Expect(t, string(body), "Rick")
+}
+
+func TestResponseModifierJSONFromStruct(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{Header: http.Header{}}
+	modifier := NewResponseModifier(req, resp)
+	u := &user{Name: "Rick"}
+	modifier.JSON(u)
+	body, err := ioutil.ReadAll(resp.Body)
+	st.Expect(t, err, nil)
+	st.Expect(t, resp.ContentLength, int64(len(body)))
+	st.Expect(t, resp.Header.Get("Content-Type"), "application/json")
+	st.Expect(t, string(body), "{\"Name\":\"Rick\"}\n")
+}
+
+func TestResponseModifierJSONFromString(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{Header: http.Header{}}
+	modifier := NewResponseModifier(req, resp)
+	input := `{"Name":"Rick"}`
+	modifier.JSON(input)
+	body, err := ioutil.ReadAll(resp.Body)
+	st.Expect(t, err, nil)
+	st.Expect(t, resp.ContentLength, int64(len(body)))
+	st.Expect(t, resp.Header.Get("Content-Type"), "application/json")
+	st.Expect(t, string(body), input)
+}
+
+func TestResponseModifierJSONFromBytes(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{Header: http.Header{}}
+	modifier := NewResponseModifier(req, resp)
+	input := []byte(`{"Name":"Rick"}`)
+	modifier.JSON(input)
+	body, err := ioutil.ReadAll(resp.Body)
+	st.Expect(t, err, nil)
+	st.Expect(t, resp.ContentLength, int64(len(body)))
+	st.Expect(t, resp.Header.Get("Content-Type"), "application/json")
+	st.Expect(t, string(body), string(input))
+}
+
+func TestResponseModifierXMLFromStruct(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{Header: http.Header{}}
+	modifier := NewResponseModifier(req, resp)
+	u := &user{Name: "Rick"}
+	modifier.XML(u)
+	body, err := ioutil.ReadAll(resp.Body)
+	st.Expect(t, err, nil)
+	st.Expect(t, resp.ContentLength, int64(len(body)))
+	st.Expect(t, resp.Header.Get("Content-Type"), "application/xml")
+	st.Expect(t, string(body), "<Person><Name>Rick</Name></Person>")
+}
+
+func TestResponseModifierXMLFromString(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{Header: http.Header{}}
+	modifier := NewResponseModifier(req, resp)
+	input := `<Person><Name>Rick</Name></Person>`
+	modifier.XML(input)
+	body, err := ioutil.ReadAll(resp.Body)
+	st.Expect(t, err, nil)
+	st.Expect(t, resp.ContentLength, int64(len(body)))
+	st.Expect(t, resp.Header.Get("Content-Type"), "application/xml")
+	st.Expect(t, string(body), input)
+}
+
+func TestResponseModifierXMLFromBytes(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{Header: http.Header{}}
+	modifier := NewResponseModifier(req, resp)
+	input := []byte(`<Person><Name>Rick</Name></Person>`)
+	modifier.XML(input)
+	body, err := ioutil.ReadAll(resp.Body)
+	st.Expect(t, err, nil)
+	st.Expect(t, resp.ContentLength, int64(len(body)))
+	st.Expect(t, resp.Header.Get("Content-Type"), "application/xml")
+	st.Expect(t, string(body), string(input))
+}
+
+func TestResponseModifierReaderFromBytesBuffer(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{}
+	modifier := NewResponseModifier(req, resp)
+	reader := bytes.NewBuffer([]byte("Hello"))
+	err := modifier.Reader(reader)
+	st.Expect(t, err, nil)
+	_, ok := resp.Body.(io.ReadCloser)
+	st.Expect(t, ok, true)
+	body, _ := ioutil.ReadAll(resp.Body)
+	st.Expect(t, string(body), "Hello")
+}
+
+func TestResponseModifierReaderFromBytesReader(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{}
+	modifier := NewResponseModifier(req, resp)
+	reader := bytes.NewReader([]byte("Hello"))
+	err := modifier.Reader(reader)
+	st.Expect(t, err, nil)
+	_, ok := resp.Body.(io.ReadCloser)
+	st.Expect(t, ok, true)
+	body, _ := ioutil.ReadAll(resp.Body)
+	st.Expect(t, string(body), "Hello")
+}
+
+func TestResponseModifierReaderFromStringReader(t *testing.T) {
+	req := &http.Request{}
+	resp := &http.Response{}
+	modifier := NewResponseModifier(req, resp)
+	reader := strings.NewReader("Hello")
+	err := modifier.Reader(reader)
+	st.Expect(t, err, nil)
+	_, ok := resp.Body.(io.ReadCloser)
+	st.Expect(t, ok, true)
+	body, _ := ioutil.ReadAll(resp.Body)
+	st.Expect(t, string(body), "Hello")
 }
